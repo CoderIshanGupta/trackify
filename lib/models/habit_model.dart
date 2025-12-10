5d8589fa071308663a7992e40f6aa0101bf0fae9
CoderIshanGupta <guptaishan2609@gmail.com>
2025-12-10 23:17:54 +0530

    Create habit_model.dart

---
+++ lib/models/habit_model.dart
@@ -0,0 +1,11 @@
+/// Model for a habit I want to track.
+///
+/// Possible fields:
+/// - id
+/// - name/title
+/// - description
+/// - frequency (daily/weekly/etc.)
+/// - streak info
+class HabitModel {
+  // TODO: define fields, constructor(s), and any helpers.
+}
