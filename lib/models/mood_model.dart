43482101fb736cf6113a163ae627fe6476956553
CoderIshanGupta <guptaishan2609@gmail.com>
2025-12-10 23:17:57 +0530

    Create mood_model.dart

---
+++ lib/models/mood_model.dart
@@ -0,0 +1,10 @@
+/// Model for a single mood log/entry.
+///
+/// I’ll probably store:
+/// - id
+/// - date/time
+/// - rating (e.g. 1–5)
+/// - optional note/tags.
+class MoodModel {
+  // TODO: define fields, constructor(s), and any helpers.
+}
