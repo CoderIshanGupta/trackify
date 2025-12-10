786047304c2fa348925b159fea634ccbbf1be249
CoderIshanGupta <guptaishan2609@gmail.com>
2025-12-10 23:17:50 +0530

    Create expense_model.dart

---
+++ lib/models/expense_model.dart
@@ -0,0 +1,12 @@
+/// Model for a single expense entry.
+///
+/// I’ll use this to represent each expense throughout the app.
+/// Rough ideas for fields (will finalize later):
+/// - id
+/// - amount
+/// - category
+/// - date
+/// - description/note
+class ExpenseModel {
+  // TODO: define fields, constructor(s), and any helpers.
+}
