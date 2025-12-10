9e9d254c7a1a5d365865b7c07d6b44a39237e657
CoderIshanGupta <guptaishan2609@gmail.com>
2025-12-10 23:19:29 +0530

    Create firebase_config.dart

---
+++ lib/services/firebase/firebase_config.dart
@@ -0,0 +1,9 @@
+/// Central place for initializing and configuring Firebase.
+///
+/// I’ll call `FirebaseConfig.initialize()` from main() before runApp(),
+/// once I add Firebase and related configuration.
+class FirebaseConfig {
+  static Future<void> initialize() async {
+    // TODO: add Firebase.initializeApp and any other setup here.
+  }
+}
