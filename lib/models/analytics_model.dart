5389e176d36bdd3144b33e1720e02090d51a6f65
CoderIshanGupta <guptaishan2609@gmail.com>
2025-12-10 23:17:47 +0530

    Create analytics_model.dart

---
+++ lib/models/analytics_model.dart
@@ -0,0 +1,10 @@
+/// Model for all the aggregated analytics/insights I want to show in the app.
+///
+/// Later I’ll add fields here for things like:
+/// - total expenses
+/// - total income/savings
+/// - category-wise breakdown
+/// - trends over time, etc.
+class AnalyticsModel {
+  // TODO: add fields, constructors, and any helper methods I need.
+}
