c4a2d2b1dd9f538da176620966f9ae29e063ba96
CoderIshanGupta <guptaishan2609@gmail.com>
2025-12-10 23:18:17 +0530

    Create task_model.dart

---
+++ lib/models/task_model.dart
@@ -0,0 +1,12 @@
+/// Model for a task / to‑do item.
+///
+/// Likely fields:
+/// - id
+/// - title
+/// - description
+/// - due date
+/// - completed flag
+/// - maybe priority.
+class TaskModel {
+  // TODO: define fields, constructor(s), and any helpers.
+}
